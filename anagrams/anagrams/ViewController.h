5a9a7c9eb1f6d03723a13fa505d418d850805529
Narcis Zait <narcis.zait@gmail.com>
2013-04-17 19:53:40 +0200

    Manage to add the board background and select it depending on the iPhone screen

--- anagrams/anagrams/ViewController.h
+++ anagrams/anagrams/ViewController.h
@@ -8,6 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+   IBOutlet UIImageView* myImageView;
+}
+
+@property (retain,nonatomic) IBOutlet UIImageView* myImageView;
 
 @end
--- anagrams/anagrams/ViewController.m
+++ anagrams/anagrams/ViewController.m
@@ -14,10 +14,28 @@
 
 @implementation ViewController
 
+@synthesize myImageView;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIImage* myImage;
+    CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
+    if ([UIScreen mainScreen].scale == 2.f && screenHeight == 568.0f) {
+        myImage = [UIImage imageNamed:@"board~568h@2x.jpg"];
+        NSLog(@"iphone 5 image");
+    } else
+        if ([UIScreen mainScreen].scale==2.f && screenHeight == 480.0f) {
+            myImage =[UIImage imageNamed:@"board@2x.jpg"];
+            NSLog(@"iphone 4 image");
+            }
+        else
+        {
+            myImage = [UIImage imageNamed:@"board.jpg"];
+            NSLog(@"iphone image");
+        }
+    [myImageView setImage:myImage];
 }
 
 - (void)didReceiveMemoryWarning
